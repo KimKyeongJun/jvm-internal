b678c2a5eda4f3e7f7f513bb4d7a9dbaee0c448f
Kyeongjun <v49011591@gmail.com>
2022-01-02 21:04:54 +0900

    코드 커버리지 사용 설정 추가(JaCoCo)

---
+++ src/main/java/com/kkj/Moim.java
@@ -0,0 +1,18 @@
+package com.kkj;
+
+public class Moim {
+
+    public int maxNumberOfAttendees;
+    public int numberOfEnrollment;
+
+    public boolean isEnrollmentFull() {
+        if (maxNumberOfAttendees == 0) {
+            return false;
+        }
+        if (numberOfEnrollment < maxNumberOfAttendees) {
+            return false;
+        }
+
+        return true;
+    }
+}
